3b9090a2e0c8454654e02ba7f6ef2f2b8ed4977f
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-05-15 14:22:11 +0300

    chore(Initialize a flask app with blueprints)

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from instance.config import app_config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])  # Configure app according to the environment
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    Migrate(app, db)
+    from .auth import auth_blueprint  # include the views, in form of blueprints
+    app.register_blueprint(auth_blueprint)
+    return app
---
+++ app/auth/__init__.py
@@ -0,0 +1,8 @@
+
+from flask import Blueprint
+
+# Instantiating a blueprint for auth
+auth_blueprint = Blueprint('auth', __name__)
+
+from . import views
+
